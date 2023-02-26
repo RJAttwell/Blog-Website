480a5d1a2a0ae1e4b298245b756c9febfe8fc163
RJAttwell <richardjattwell@gmail.com>
2023-02-26 00:52:03 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
+const { lowerCase } = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,30 +19,30 @@ const posts = [];
 app.set('view engine', 'ejs');
 
 //Commands that allow us to use body-parser and EJS
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-  res.render("home", {homeStartingContent});
+app.get("/", function (req, res) {
+  res.render("home", { posts: posts });
 
 });
 
 //Forms the about us page
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent });
 });
 
 //Forms the contact us page
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent });
 });
 
 //Forms the compose page
-app.get("/compose", function(req, res){
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
+app.post("/compose", function (req, res) {
   //Create a JS Object to store the multiple pieces of data from the compose page
   const content = {
     title: req.body.postTitle,
@@ -54,7 +56,33 @@ app.post("/compose", function(req, res){
   res.redirect("/")
 })
 
+app.get("/zoinks", function(req, res){
+  res.render("zoinks");
+});
+
+app.get("/post", function(req, res){
+
+  res.render("post", { posts: posts });
+});
+
+//Using route parameters to capture the value in the URL
+app.get("/posts/:postName", function (req, res) {
+  const requestedName = _.lowerCase(req.params.postName);
+
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+
+    //If statement to see if the post title is the same as the URL Parameter 
+    if (storedTitle === requestedName) {
+      res.redirect("/posts/:postName");
+    } else {
+      res.redirect("/zoinks");
+    }
+  });
+
+});
+
 //Starts server on port 3000
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
